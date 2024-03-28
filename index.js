7731ec5051c57015e4ba4a124565596c09989c52
uie64386 <daniel.filipe.silva@conti.de>
2024-03-28 09:41:55 +0000

    remove of redundant code

--- index.js
+++ index.js
@@ -5,19 +5,12 @@
         var countries = document.querySelectorAll(".country");
 
         countries.forEach(function(country) {
-            // Hover effect
-            country.addEventListener("mouseenter", function() {
-                this.setAttribute("fill", "#aaa");
-            });
-
-            country.addEventListener("mouseleave", function() {
-                this.setAttribute("fill", "#ccc");
-            });
-
+			
             // Click action
             country.addEventListener("click", function() {
                 var countryId = this.id;
-                // Perform your action here, e.g., display information about the country
+                
+				// perform action
                 alert("You clicked on " + countryId);
             });
         });
